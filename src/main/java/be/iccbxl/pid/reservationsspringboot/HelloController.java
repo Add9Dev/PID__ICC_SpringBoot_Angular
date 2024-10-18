7b84625dc2dbcbee29da4e1ea184c08f4afa2292
Adnane <add9dev@gmail.com>
2024-10-18 16:50:38 +0200

    Controller Artist +templates +routes

--- src/main/java/be/iccbxl/pid/reservationsspringboot/HelloController.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/HelloController.java
@@ -8,6 +8,8 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
+		String s = "vive intellij";
+		System.out.println(s);
 		return "Greetings from Spring Boot!";
 	}
 
