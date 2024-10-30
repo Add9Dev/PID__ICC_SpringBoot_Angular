07c2775f2de23372e42e99195e8e60d7533200c9
Adnane <add9dev@gmail.com>
2024-10-30 01:31:09 +0100

    Form create Artist

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/Artist.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/Artist.java
@@ -1,12 +1,13 @@
 package be.iccbxl.pid.reservationsspringboot.model;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name="artists")
@@ -15,15 +16,16 @@ public class Artist {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "The firstname must not be empty.")
+    @NotEmpty(message = "The firstname must not be empty.")
     @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
     private String firstname;
 
-    @NotBlank(message = "The lastname must not be empty.")
+    @NotEmpty(message = "The lastname must not be empty.")
     @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
     private String lastname;
 
     protected Artist() {}
+
 
     public Artist(String firstname, String lastname) {
         this.firstname = firstname;
