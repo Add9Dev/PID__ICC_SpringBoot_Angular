8e8cd139ad385cae8f93e682d9b0cc53f660f07b
Adnane <add9dev@gmail.com>
2025-01-08 10:39:04 +0100

    Relation ManyToMany between Artist & Type

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/Artist.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/Artist.java
@@ -1,16 +1,14 @@
 package be.iccbxl.pid.reservationsspringboot.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import org.hibernate.validator.constraints.NotEmpty;
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -25,9 +23,32 @@ public class Artist {
 
     @NotEmpty(message = "The firstname must not be empty.")
     @Size(min = 2, max = 60, message = "The firstname must be between 2 and 60 characters long.")
+    @Column(nullable = false)
     private String firstname;
 
     @NotEmpty(message = "The lastname must not be empty.")
     @Size(min = 2, max = 60, message = "The lastname must be between 2 and 60 characters long.")
+    @Column(nullable = false)
     private String lastname;
+
+    @ManyToMany(mappedBy = "artists")
+    private List<Type> types = new ArrayList<>();
+
+    public void addType(Type type) {
+        if (!this.types.contains(type)) {
+            this.types.add(type);
+            type.addArtist(this); // Mise à jour bidirectionnelle
+        }
+    }
+
+    public void removeType(Type type) {
+        if (this.types.remove(type)) {
+            type.getArtists().remove(this); // Mise à jour bidirectionnelle
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Artist [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", types=" + types.size() + "]";
+    }
 }
--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/Type.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/Type.java
@@ -1,14 +1,12 @@
 package be.iccbxl.pid.reservationsspringboot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -21,9 +19,35 @@ public class Type {
     private Long id;
 
     @NotEmpty(message = "The type name must not be empty.")
+    @Column(nullable = false, unique = true) // Ajout de contraintes pour la base de données
     private String type;
+
+    @ManyToMany
+    @JoinTable(
+            name = "artist_type",
+            joinColumns = @JoinColumn(name = "type_id"),
+            inverseJoinColumns = @JoinColumn(name = "artist_id"))
+    private List<Artist> artists = new ArrayList<>();
 
     public Type(String type) {
         this.type = type;
+    }
+
+    public void addArtist(Artist artist) {
+        if (!this.artists.contains(artist)) {
+            this.artists.add(artist);
+            artist.addType(this); // Mise à jour bidirectionnelle
+        }
+    }
+
+    public void removeArtist(Artist artist) {
+        if (this.artists.remove(artist)) {
+            artist.getTypes().remove(this); // Mise à jour bidirectionnelle
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Type [id=" + id + ", type=" + type + ", artists=" + artists.size() + "]";
     }
 }
